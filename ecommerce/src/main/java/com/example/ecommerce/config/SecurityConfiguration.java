ee93d0d99e704d26364bdf7ce84e505e4df84c8c
Keerthana M <keerthanakrishnan124@gmail.com>
2024-08-15 10:09:58 +0530

    Update SecurityConfiguration

--- ecommerce/src/main/java/com/example/ecommerce/config/SecurityConfiguration.java
+++ ecommerce/src/main/java/com/example/ecommerce/config/SecurityConfiguration.java
@@ -3,13 +3,15 @@ package com.example.ecommerce.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -17,6 +19,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.example.ecommerce.service.MyUserDetailsService;
 
@@ -24,8 +27,12 @@ import com.example.ecommerce.service.MyUserDetailsService;
 
 
 @Configuration
+@EnableMethodSecurity
 @EnableWebSecurity
 public class SecurityConfiguration {
+	
+	@Autowired
+	private JwtFilter jwtFilter;
 	
 	@Autowired
 	private MyUserDetailsService myUserDetailsServie;
@@ -33,21 +40,18 @@ public class SecurityConfiguration {
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 		return httpSecurity
-				.csrf(AbstractHttpConfigurer::disable)
-			.authorizeHttpRequests(registry->{
-			registry.requestMatchers("/home","/register/**").permitAll();
-			registry.requestMatchers("/admin/**").hasRole("ADMIN");
-			registry.requestMatchers("/user/**").hasRole("USER");
-			registry.anyRequest().authenticated();
-		})
-		.formLogin(httpSecurityFormLoginConfigurer -> {
-			httpSecurityFormLoginConfigurer
-//			.loginPage("/login")
-			.successHandler(new AuthenticationSuccessHandler())
-			.permitAll();
-		})
-		 .httpBasic(Customizer.withDefaults())
-		.build();
+				.csrf(customizer -> customizer.disable())
+			.authorizeHttpRequests(registry->
+			registry
+			.requestMatchers("/home","/register","/login").permitAll()
+//			.requestMatchers("/admin/**").hasRole("ADMIN")
+//			.requestMatchers("/user/**").hasRole("USER")
+			.anyRequest().authenticated())
+			.httpBasic(Customizer.withDefaults())
+			.sessionManagement(session-> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+			.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+			.build();
+	
 	}
 	
 //	@Bean
@@ -60,7 +64,7 @@ public class SecurityConfiguration {
 //		UserDetails adminUser=User.builder()
 //				.username("admin")
 //				.password("$2a$12$HnLvHzbpMYnY4LIhOvehlONjMx1qVmWMm4IZ9BYU8GDWsYag9RRiW")
-//				.roles("ADMIN","USER")
+//				.roles("ADMIN")
 //				.build();
 //		return new InMemoryUserDetailsManager(normalUser,adminUser);
 //	}
@@ -79,8 +83,14 @@ public class SecurityConfiguration {
 		return provider;
 	}
 	
+	    @Bean
+	    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+	        return config.getAuthenticationManager();
+
+	    }
+	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
+		return new BCryptPasswordEncoder(12);
 	}
 }
